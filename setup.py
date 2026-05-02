32a463eccab9d3c932a71ce413f36583889f3edc
Aashish <mungaseashu02@gmail.com>
2026-05-02 18:29:48 +0530

    created folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project Medical Chatbot',
+    version= '0.0.0',
+    author= 'Aashish Mungase',
+    author_email= 'mungaseashu02@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
